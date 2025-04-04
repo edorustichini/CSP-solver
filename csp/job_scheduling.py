6e8a2a2f665e2b0ebac2e3bdba9771d3a3bf4ebf
edo <edorusti@gmail.com>
2025-04-04 09:00:39 +0200

    Added description of the project

--- csp/job_scheduling.py
+++ csp/job_scheduling.py
@@ -2,10 +2,7 @@
 Job-shop scheduling problems:
 The variables represent the operations,
 """
-from typing import override
-
 from csp.constraint import BinaryConstraint
-from csp.solver import Solver
 from problem import Problem
 
 class JobShopSchedulingProblem(Problem):
@@ -55,68 +52,3 @@ class JobShopSchedulingProblem(Problem):
         func =  lambda a,b : (a + x<= b) or (b + y <= a)
         c = BinaryConstraint([A,B], func)
         self.add_constraint(c)
-        
-        
-    
-
-def main():
-    # Problem described in AIMA 2021 RN  chap: 6.1.2
-    duration ={
-        'Asse_A': 10,
-        'Asse_P': 10,
-        'Ruota_DA': 3,
-        'Ruota_SA':3,
-        'Ruota_DP': 3,
-        'Ruota_SP':3,
-        'Dadi_DA': 2,
-        'Dadi_SA': 2,
-        'Dadi_DP': 2,
-        'Dadi_SP': 2,
-        'Copri_DA': 1,
-        'Copri_SA':1,
-        'Copri_DP':1,
-        'Copri_SP':1,
-        'Ispezione':3
-    }
-    operations = list(duration.keys())
-    
-    jss_problem = JobShopSchedulingProblem(operations,30,  duration)
-    jss_problem.add_precedence_constraint('Asse_A', 'Ruota_DA')
-    jss_problem.add_precedence_constraint('Asse_P', 'Ruota_DP')
-    jss_problem.add_precedence_constraint('Asse_A', 'Ruota_SA')
-    jss_problem.add_precedence_constraint('Asse_P', 'Ruota_SP')
-    
-    jss_problem.add_precedence_constraint('Ruota_DA', 'Dadi_DA')
-    jss_problem.add_precedence_constraint('Ruota_SA', 'Dadi_SA')
-    jss_problem.add_precedence_constraint('Ruota_DP', 'Dadi_DP')
-    jss_problem.add_precedence_constraint('Ruota_SP', 'Dadi_SP')
-    
-    jss_problem.add_precedence_constraint('Dadi_DA', 'Copri_DA')
-    jss_problem.add_precedence_constraint('Dadi_SA', 'Copri_SA')
-    jss_problem.add_precedence_constraint('Dadi_DP', 'Copri_DP')
-    jss_problem.add_precedence_constraint('Dadi_SP', 'Copri_SP')
-    
-    jss_problem.add_disjunction_constraint('Asse_A', 'Asse_P')
-    
-    for op in operations:
-        if op != 'Ispezione':
-            jss_problem.add_precedence_constraint(op, 'Ispezione')
-    
-    
-    solver = Solver(jss_problem)
-    
-    
-    solutions = solver.get_all_solutions()
-    
-    print("Some solutions")
-    num = 5
-    for i, solution in enumerate(solutions[:num], start=1):
-        print(f"Solution n. {i} :")
-        for op, value in solution.items():
-            print(f"{op} = {value}")
-        print("------------------------------------------------------------------------- \n")
-    print(f".... other {len(solutions) - 5} found")
-    
-    #TODO: SCEGLIERE UNA SOLUZIONE A COSTO MINIMO
-        
-main()
