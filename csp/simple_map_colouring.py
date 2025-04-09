78a417779f15af172604d89ae76bf848b832343c
edo <edorusti@gmail.com>
2025-04-09 22:57:10 +0200

    Removed main() function

--- csp/simple_map_colouring.py
+++ csp/simple_map_colouring.py
@@ -2,57 +2,47 @@ from constraint import BinaryConstraint
 from problem import Problem
 from solver import Solver
 
+print("Australia map colouring")
 
-def main():
-    # 1. Creazione del problema
-    print("Creazione problema CSP di colorazione mappa...")
-    
-    domain = ['Rosso', 'Verde', 'Blu']
-     #TODO: aggiungere var T (Tamzamia?) come nel libro, che è variabile senza vincoli
-    problem = Problem()
-    problem.add_variables_same_domain(domain, ['WA', 'NT','Q','NSW','V','SA'])
-    
-    print("Adding constraints...")
-    #FIXME: si dovbrebbero mettere i vincoli in tutte e due i varsi
-    constraints = [
-        # in this problem the constraints are symmetric
-        BinaryConstraint(('SA', 'WA'), lambda a, b: a != b),
-        
-        BinaryConstraint(('SA', 'NT'), lambda a, b: a != b),
-        
-        BinaryConstraint(('SA', 'Q'), lambda a, b: a != b),
-        
-        BinaryConstraint(('SA', 'NSW'), lambda a, b: a != b),
-        
-        BinaryConstraint(('SA', 'V'), lambda a, b: a != b),
-        
-        BinaryConstraint(('WA', 'NT'), lambda a, b: a != b),
-        
-        BinaryConstraint(('NT', 'Q'), lambda a, b: a != b),
-        
-        BinaryConstraint(('Q', 'NSW'), lambda a, b: a != b),
-        
-        BinaryConstraint(('NSW', 'V'), lambda a, b: a != b),
-    ]
-    
-    print("Vincoli:")
-    for c in constraints:
-        problem.add_constraint(c)
-        print(f"{c.variables}")
-    
-    
-    
-    print("Risoluzione con backtracking + MAC...")
-    solver = Solver(problem)
-    solutions = solver.backtracking_search()
-    
-    #  Solutions
-    print("\nSoluzioni trovate:", len(solutions))
-    for i, sol in enumerate(solutions, 1):
-        print(f"\nSoluzione {i}:")
-        for var, val in sorted(sol.items()):
-            print(f"{var}: {val}")
-    
-main()
+domain = ['Rosso', 'Verde', 'Blu']
+problem = Problem()
+problem.add_variables_same_domain(domain, ['WA', 'NT','Q','NSW','V','SA'])
 
+print("Adding constraints...")
+constraints = [ #neibouring regions must have different colors
+    BinaryConstraint(('SA', 'WA'), lambda a, b: a != b),
+    
+    BinaryConstraint(('SA', 'NT'), lambda a, b: a != b),
+    
+    BinaryConstraint(('SA', 'Q'), lambda a, b: a != b),
+    
+    BinaryConstraint(('SA', 'NSW'), lambda a, b: a != b),
+    
+    BinaryConstraint(('SA', 'V'), lambda a, b: a != b),
+    
+    BinaryConstraint(('WA', 'NT'), lambda a, b: a != b),
+    
+    BinaryConstraint(('NT', 'Q'), lambda a, b: a != b),
+    
+    BinaryConstraint(('Q', 'NSW'), lambda a, b: a != b),
+    
+    BinaryConstraint(('NSW', 'V'), lambda a, b: a != b),
+]
+
+print("Adding constraints...:")
+for c in constraints:
+    problem.add_constraint(c)
+    
+
+print("Backtracking...")
+solver = Solver(problem)
+solutions = solver.backtracking_search()
+
+#  Solutions
+print(f"\nFound {len(solutions)} solutions")
+for i, sol in enumerate(solutions, start=1):
+    print(f"\nSol. {i}:")
+    for var, val in sorted(sol.items()):
+        print(f"{var}: {val}")
+    
     
